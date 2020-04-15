68812c496ad7999c1f9502eb91ca060224d12fc2
MichaelRostom <46191685+MichaelRostom@users.noreply.github.com>
2020-04-15 22:47:44 +0300

    Initial setup

---
+++ Univariate Linear Regression.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+Raw_Data = np.genfromtxt('ex1data1.txt', delimiter=',')  # loads the data from the file
+
+m = Raw_Data.shape[0]  # Sets the length
+
+Y = np.ones((m, 1))
+Y[:, 0] = Raw_Data[:, 1]
+X = np.ones((m, 2))
+X[:, 1] = Raw_Data[:, 0]  # adds the column of ones to the x's
+
+theta = np.zeros((2, 1))  # Parameters
+# settings
+alpha = 0.01
+itertions = 1500
+
+
+# cost function
+def Cost_Function(X, Y, theta) :
+    squared_err = (np.subtract(np.matmul(X, theta), Y)) ** 2  #
+    return 1 / (2 * m) * squared_err.sum()
+
+
+# Gradient Descent
+
+for iter in range(itertions) :
+    Delta = np.ones((X.shape[1], 1))
+    for j in range(X.shape[1]) :
+        op = np.subtract(np.matmul(X, theta), Y).transpose()  # true
+        Delta[j, 0] = 1/m*np.matmul(op , X[:, j])
+    theta = np.subtract(theta , alpha * Delta)
+print(theta)
